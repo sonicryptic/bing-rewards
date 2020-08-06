b9857e3dd3f8873b1e60aebc603a38d134ac74e3
Sonic Cryptic <sonicryptic@gmail.com>
2020-08-06 12:50:38 -0700

    Updates

---
+++ precache-manifest.25178c798f698964ac8942f42d68a731.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b441b2215ca33468d89f2e4eaeec79d3",
+    "url": "/bing-rewards/index.html"
+  },
+  {
+    "revision": "667ec1caee832ea171eb",
+    "url": "/bing-rewards/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ab30087787b4b28568ad",
+    "url": "/bing-rewards/static/js/2.aaac930e.chunk.js"
+  },
+  {
+    "revision": "d74cc00b855f3aae463dfec757fcf24f",
+    "url": "/bing-rewards/static/js/2.aaac930e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "667ec1caee832ea171eb",
+    "url": "/bing-rewards/static/js/main.05387419.chunk.js"
+  },
+  {
+    "revision": "7b152b6e5a67ca45310b",
+    "url": "/bing-rewards/static/js/runtime-main.aa156cd8.js"
+  }
+]);
