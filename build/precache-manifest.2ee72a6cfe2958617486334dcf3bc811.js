6da3bd7bc76977688ea2008a33d0db0b8d5678b4
Sonic Cryptic <63802016+sonicryptic@users.noreply.github.com>
2020-08-06 12:28:05 -0700

    Adding build folder to repo for github pages. (#2)

---
+++ build/precache-manifest.2ee72a6cfe2958617486334dcf3bc811.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4def27579076dc8fbb89c8d1daa8009e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "667ec1caee832ea171eb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ab30087787b4b28568ad",
+    "url": "/static/js/2.aaac930e.chunk.js"
+  },
+  {
+    "revision": "d74cc00b855f3aae463dfec757fcf24f",
+    "url": "/static/js/2.aaac930e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "667ec1caee832ea171eb",
+    "url": "/static/js/main.05387419.chunk.js"
+  },
+  {
+    "revision": "cd34d94a73809114aa29",
+    "url": "/static/js/runtime-main.cef7f77f.js"
+  }
+]);
